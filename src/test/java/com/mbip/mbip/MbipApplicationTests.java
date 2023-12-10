0c3d44fbf2a9c8a888efc316e492fe65b5781833
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-12-10 20:03:54 +0800

    Initial commit

---
+++ src/test/java/com/mbip/mbip/MbipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbip.mbip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
